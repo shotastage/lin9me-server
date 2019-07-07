973c39e0004caebf2b80abc8b41defef78d5b036
Shota Shimazu <hornet.live.mf@gmail.com>
2019-07-08 00:11:51 +0900

    revise

--- config/config.go
+++ config/config.go
@@ -3,7 +3,6 @@ package config
 import (
 	"encoding/json"
 	"io/ioutil"
-	"os"
 )
 
 // Config is global variables for sharing a configuration value
@@ -12,23 +11,25 @@ var Config *AppConfig
 var err error
 
 // AppConfig is struct that define configuration file structure
+
 type AppConfig struct {
 	Database struct {
 		Host     string `json:"host"`
-		Port     string `json:"port"`
 		Name     string `json:"name"`
-		User     string `json:"user"`
 		Password string `json:"password"`
+		Port     string `json:"port"`
 		Ssl      string `json:"ssl"`
+		User     string `json:"user"`
 	} `json:"database"`
+	Host      string `json:"host"`
+	JwtConfig struct {
+		Sig string `json:"sig"`
+	} `json:"jwt_config"`
+	Port   string `json:"port"`
+	Ssl    string `json:"ssl"`
 	Static struct {
 		Directory string `json:"directory"`
 	} `json:"static"`
-	JWTConfig struct {
-	} `json:"jwt_config"`
-	Host string `json:"host"`
-	Port string `json:"port"`
-	Ssl  string `json:"ssl"`
 }
 
 // Init is a functino  for configuraing and share all modules
@@ -37,17 +38,6 @@ func Init(fname string) error {
 	c := new(AppConfig)
 
 	jsonString, err := ioutil.ReadFile(fname)
-
-	// Priority for environmental value
-	configPriority := os.Getenv("CONFIG_PRIORITY")
-
-	if configPriority == "String::EnvironmentalValue" {
-		c.Database.Host = os.Getenv("DATABASE_HOST")
-		c.Database.Port = os.Getenv("DATABASE_PORT")
-		c.Database.Name = os.Getenv("DATABASE_NAME")
-		c.Database.User = os.Getenv("DATABASE_USER")
-		c.Database.Password = os.Getenv("DATABASE_PASS")
-	}
 
 	// File load error
 	if err != nil {
