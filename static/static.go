f5d201c05fb4ecb910f279e922f75669c12b041f
Shota Shimazu <hornet.live.mf@gmail.com>
2019-07-07 09:51:29 +0900

    revise

--- static/static.go
+++ static/static.go
@@ -1,7 +1,18 @@
 package static
 
-func Init(static_dir string) {
+import (
+	"fmt"
+
+	"lin9.me/config"
+)
+
+func Init(static_dir string) string {
 	fmt.Println("Initializing static files environment...")
+	config := config.Config.Static.Directory
+
+	return config
 
 }
 
+type StaticFIle struct {
+}
