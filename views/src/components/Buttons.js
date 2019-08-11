d50986c87678aab54c1d4e566b8bf161490ca4a9
Shota Shimazu <hornet.live.mf@gmail.com>
2019-08-11 23:51:01 +0900

    update design

--- views/src/components/Buttons.js
+++ views/src/components/Buttons.js
@@ -16,24 +16,13 @@ export const Button = styled.button`
     text-transform: uppercase;
     transition: 0.5s;
     color: white;
-    border-radius: 30px;
-    background: linear-gradient(-45deg, rgb(255, 151, 224) 0%, rgb(255, 0, 76) 100%);
-    box-shadow: 0 5px 10px rgba(255, 0, 76, .5);
+    border-radius: 15px;
+    background: #d43333;
     font-weight: bold;
     font-size: 1.5rem;
     font-family: 'Roboto Condensed', sans-serif;
 
     margin: 25px;
-    &:hover {
-        $color: rgb(255, 0, 76);
-        animation: halftone 1s forwards;
-        background: radial-gradient(circle, $color 0.2em, transparent 0.25em) 0 0 / 1.25em 1.25em
-              , radial-gradient(circle, $color 0.2em, transparent 0.25em) 6.25em 6.25em / 1.25em 1.25em;
-        color: adjust-hue($color ,180);
-        box-shadow: none;
-        text-decoration: none;
-        color: #fff;
-    }
 
     &:focus {
 	    outline:0;
--- views/src/components/Input.js
+++ views/src/components/Input.js
@@ -5,13 +5,13 @@ appearance: none;
 border: none;
 height: 60px;
 width: 400px;
-border-radius: 30px;
+border-radius: 14px;
 padding-left: 30px;
 letter-spacing: 1px;
 font-size: 1.5rem;
 font-family: 'Roboto Condensed', sans-serif;
 font-weight: bold;
-box-shadow: 0 5px 10px rgba(78, 78, 78, 0.5);
+background: #ebebeb;
 
 &:focus {
     outline: 0;
